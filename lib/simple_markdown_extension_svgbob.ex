5c2471f2e5568983f47791d6b396adee669795d4
Stefan Johnson <sj@scrimpycat.io>
2017-01-26 22:11:43 +1100

    Include module doc

--- lib/simple_markdown_extension_svgbob.ex
+++ lib/simple_markdown_extension_svgbob.ex
@@ -1,4 +1,16 @@
 defmodule SimpleMarkdownExtensionSvgBob do
+    @moduledoc """
+      This extension assumes the default HTML rendering behaviour
+      for `SimpleMarkdown.Attribute.PreformattedCode` has not been
+      overriden.
+
+      If it has then the new implementation must pass the call to
+      `SimpleMarkdown.Attribute.PreformattedCode.Svgbob`.
+
+      Otherwise if it has not overriden the implementation, then
+      no extra work is needed.
+    """
+
     defimpl SimpleMarkdown.Renderer.HTML, for: SimpleMarkdown.Attribute.PreformattedCode.Svgbob do
         def render(%{ input: input }) do
             SimpleMarkdown.Renderer.HTML.render(input)
