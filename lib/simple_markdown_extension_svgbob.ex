948f0d0588ef48937bd8774783b5d9f390d5939f
Stefan Johnson <sj@scrimpycat.io>
2017-01-26 23:28:55 +1100

    Remove encoding html elements

--- lib/simple_markdown_extension_svgbob.ex
+++ lib/simple_markdown_extension_svgbob.ex
@@ -14,7 +14,6 @@ defmodule SimpleMarkdownExtensionSvgBob do
     defimpl SimpleMarkdown.Renderer.HTML, for: SimpleMarkdown.Attribute.PreformattedCode.Svgbob do
         def render(%{ input: input }) do
             SimpleMarkdown.Renderer.HTML.render(input)
-            |> HtmlEntities.encode
             |> SvgBobEx.to_svg!
         end
     end
