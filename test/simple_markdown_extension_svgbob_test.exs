2a518fb5816910fd4f89109a80d14372912cd7b9
Stefan Johnson <sj@scrimpycat.io>
2017-01-26 22:09:34 +1100

    Add HTML renderer to convert svgbob code snippets to svg

--- test/simple_markdown_extension_svgbob_test.exs
+++ test/simple_markdown_extension_svgbob_test.exs
@@ -1,3 +1,14 @@
 defmodule SimpleMarkdownExtensionSvgBobTest do
     use ExUnit.Case
+
+    test "Successfully converts svgbob code snippets to svg" do
+        src = """
+        ```svgbob
+        +-----+    +-----+
+        | one +--->| two |
+        +-----+    +-----+
+        ```
+        """
+        assert <<"<svg", _ :: binary>> = SimpleMarkdown.convert(src)
+    end
 end
