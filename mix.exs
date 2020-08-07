72b8b27a0fc56d2dff327e7a03170e4a793949ef
Stefan Johnson <sj@scrimpycat.io>
2020-08-08 06:46:13 +1000

    Update to 0.1.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SimpleMarkdownExtensionSvgBob.Mixfile do
         [
             app: :simple_markdown_extension_svgbob,
             description: "An extension for SimpleMarkdown to add a renderer for svgbob code snippets.",
-            version: "0.1.0",
+            version: "0.1.1",
             elixir: "~> 1.3",
             build_embedded: Mix.env == :prod,
             start_permanent: Mix.env == :prod,
